189724fc412baec36a1cb6fd814c90a44ef3225e
genxhero <genxhero@users.noreply.github.com>
2020-03-14 04:01:49 -0700

    alert actions redux fix

--- src/actions/alert.actions.js
+++ src/actions/alert.actions.js
@@ -1,4 +1,4 @@
-import { alertConstants } from '../constants';
+import { alertConstants } from '../constants/alert.constants';
 
 export const alertActions = {
     success,
@@ -7,10 +7,13 @@ export const alertActions = {
 };
 
 function success(message) {
+    return { type: alertConstants.SUCCESS, message };
 }
 
 function error(message) {
+    return { type: alertConstants.ERROR, message };
 }
 
 function clear() {
+    return { type: alertConstants.CLEAR };
 }
