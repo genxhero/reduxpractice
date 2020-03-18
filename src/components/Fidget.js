2f86216904d6f3abe5b9e8ec5b58348604c52b6d
genxhero <genxhero@users.noreply.github.com>
2020-03-17 22:50:46 -0700

    set color to variable

--- src/components/Fidget.js
+++ src/components/Fidget.js
@@ -2,6 +2,14 @@ import React, {useState} from 'react';
 
 const Fidget = (props) => {
 
+    const [color, setColor] = useState("red");
+    return (
+        <div style={{"background": color}}>
+            Click Me
+        </div>
+    )
+
+
 }
 
 export default Fidget;
