7a8e3f92dbc053a3fbdeddf2ca8933e50b13ffc3
genxhero <genxhero@users.noreply.github.com>
2020-03-17 22:57:13 -0700

    make the cycle of colors

--- src/components/Fidget.js
+++ src/components/Fidget.js
@@ -1,13 +1,19 @@
 import React, {useState} from 'react';
 
+const COLORS = ["red", "blue", "green", "cyan", "magenta", "yellow", "purple"]
+
 const Fidget = (props) => {
 
     const changeColor = ()=> {
-        setColor("green")
+        if (color < COLORS.length) {
+            setColor(color + 1)
+        } else {
+            setColor(0)
+        }
     }
-    const [color, setColor] = useState("red");
+    const [color, setColor] = useState(0);
     return (
-        <div style={{"background": color}} onClick={changeColor}>
+        <div style={{"background": COLORS[color]}} onClick={changeColor}>
             Click Me
         </div>
     )
